60bb46eb686544d83c067c76b4323eac9b108aaa
nbrr <nbrr@protonmail.com>
2020-04-07 09:35:24 +0200

    Add configuration options to the daemon.

--- examples/daemon.rs
+++ examples/daemon.rs
@@ -5,34 +5,45 @@ use std::fs;
 use std::path::Path;
 
 use log::debug;
-use simple_logger;
+use env_logger;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Params {
+    #[structopt(default_value="31", help="brightness, from 0 to 31")]
+    brightness: u8,
+    #[structopt(default_value="255")]
+    intensity: u8,
+    #[structopt(default_value="53", help="temperature under which the fan will stop")]
+    rest_temp: u32,
+    #[structopt(default_value="55", help="temperature over which the fan will start")]
+    blow_temp: u32,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init().unwrap();
-    const REST_TEMP: u32 = 53000;
-    const BLOW_TEMP: u32 = 55000;
-    const BRIGHTNESS: f32 = 0.04;
+    env_logger::init();
+
+    let params = Params::from_args();
 
     let path = Path::new("/sys/devices/virtual/thermal/thermal_zone0/temp");
     let mut fanshim = Fanshim::default_config()?;
     fanshim.fan_on();
     debug!("fanshim intialized");
-    fanshim.led_off();
-    fanshim.color(1., 255, 255, 255);
 
     loop {
         let thermal_data = fs::read_to_string(&path);
         let temp = thermal_data?.trim().parse::<u32>()?;
         debug!("temperature read: {}", temp);
 
-        if temp <= REST_TEMP {
+        if temp <= params.rest_temp*1000 {
             fanshim.fan_off();
-            fanshim.color(BRIGHTNESS, 0, 255, 0);
-        } else if temp >= BLOW_TEMP {
+            fanshim.color(params.brightness, 0, params.intensity, 0);
+        } else if temp >= params.blow_temp*1000 {
             fanshim.fan_on();
-            fanshim.color(BRIGHTNESS, 255, 0, 0);
+            fanshim.color(params.brightness, params.intensity, 0, 0);
         } else {
-            fanshim.color(BRIGHTNESS, 255, 255, 0);
+            fanshim.color(params.brightness, params.intensity, params.intensity, 0);
         }
     }
 }
